2979add644dff5ebd22acef84c35df43a3f9cbdb
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-09-30 17:27:27 +0300

    unit 2

--- script.js
+++ script.js
@@ -13,7 +13,31 @@ let log = console.log;
 
 // log(sumAll([1, 4]));
 
-///////////////////////////////////// unit 1 ///////////////////////////////
+///////////////////////////////////// unit 2 ///////////////////////////////
+
+// function diffArray(arr1, arr2) {
+//   let newArr = [];
+//   if (arr1.length == 0 || arr2.length == 0) {
+//     if (arr1.length > 0) { newArr = arr1 }
+//     else { newArr = arr2 }
+//   }
+//   else {
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (!arr2.includes(arr1[i])) {
+//         newArr.push(arr1[i]);
+//       }
+//       for (let i = 0; i < arr2.length; i++) {
+//         if (!arr1.includes(arr2[i])) {
+//           newArr.push(arr2[i]);
+//         }
+//       }
+//     }
+//   }
+
+//   [...newArr] = new Set(newArr);
+//   return newArr;
+// }
+// log(diffArray([], ["snuffleupagus", "cookie monster", "elmo"]));
 
 ///////////////////////////////////// unit 1 ///////////////////////////////
 
