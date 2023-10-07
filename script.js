099fbe36c5f8f89a96c7d5df2a94a94d46c128ac
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-10-07 16:50:30 +0300

    redoing unit 14

--- script.js
+++ script.js
@@ -247,45 +247,33 @@ let log = console.log;
 
 ///////////////////////////////////// unit 14 ///////////////////////////////
 
-function smallestCommons(arr) {
-  arr = arr.sort((a, b) => a - b);
-  let temp = [];
-  for (let i = arr[0]; i <= arr[1]; i++) {
-    temp.push(i);
-  }
-  return temp.reduce((el, akkum) => { return akkum *= el });
+// function smallestCommons(arr) {
+//   let [min, max] = arr.sort((a, b) => a - b);
+//   let temp = [];
 
-}
+//   for (let i = min; i <= max; i++) {
+//     temp.push(i);
+//   }
+//   log(temp.length);
+//   let amount = temp.reduce((el, akkum) => { return akkum *= el });
 
-log(smallestCommons([1, 5]));
+//   for (let i = max; i <= amount; i += max) {
+//     let count = 0;
+//     for (let k = min; k <= max; k++) {
+//       if (i % k === 0) {
+//         count++;
+//       }
+//     }
+//     if (temp.length === count) {
+//       return i;
+//     }
+//   }
+// }
+
+// log(smallestCommons([23, 18]));
 
 ///////////////////////////////////// unit 15 ///////////////////////////////
 
 ///////////////////////////////////// unit 16 ///////////////////////////////
 
 ///////////////////////////////////// unit 17 ///////////////////////////////
-
-// function smallestCommons(arr) {
-//   // Setup
-//   const [min, max] = arr.sort((a, b) => a - b);
-//   const numberDivisors = max - min + 1;
-//   // Largest possible value for SCM
-//   let upperBound = 1;
-//   for (let i = min; i <= max; i++) {
-//     upperBound *= i;
-//   }
-//   // Test all multiples of 'max'
-//   for (let multiple = max; multiple <= upperBound; multiple += max) {
-//     // Check if every value in range divides 'multiple'
-//     let divisorCount = 0;
-//     for (let i = min; i <= max; i++) {
-//       // Count divisors
-//       if (multiple % i === 0) {
-//         divisorCount += 1;
-//       }
-//     }
-//     if (divisorCount === numberDivisors) {
-//       return multiple;
-//     }
-//   }
-// }
